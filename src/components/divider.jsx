b22c4b191e582fb38e23483a8a01df1e621c889f
randyer <rbdyer3@gmail.com>
2024-09-18 22:57:58 -0400

    updated divider styling

--- src/components/divider.jsx
+++ src/components/divider.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Divider = ({ children }) => {
   return (
     <div className="flex justify-center items-center m-8 animate-fade-in duration-700">
-      <div className="flex-grow border-t-2 border-gold max-w-xl "></div>
-      <span className="mx-4 text-xl text-gold">{children}</span>
-      <div className="flex-grow border-t-2 border-gold max-w-xl"></div>
+      <div className="flex-grow border-t-2 border-gold max-w-xl rounded "></div>
+      <span className="mx-4 text-2xl text-gold">{children}</span>
+      <div className="flex-grow border-t-2 border-gold max-w-xl rounded"></div>
     </div>
   );
 };
