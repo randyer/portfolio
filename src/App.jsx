a08452784be4a27ecd1151241e482fb6916ef3cd
randyer <rbdyer3@gmail.com>
2024-10-01 09:12:17 -0400

    removed smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,23 @@ import Header from "./sections/Header";
 import Footer from "./sections/Footer";
 
 // Libraries
-import Lenis from "lenis";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+// import Lenis from "lenis";
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
 
-const lenis = new Lenis();
+// const lenis = new Lenis();
 
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
+// lenis.on("scroll", (e) => {
+//   console.log(e);
+// });
 
-lenis.on("scroll", ScrollTrigger.update);
+// lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
+// gsap.ticker.add((time) => {
+//   lenis.raf(time * 1000);
+// });
 
-gsap.ticker.lagSmoothing(0);
+// gsap.ticker.lagSmoothing(0);
 
 function App() {
   const projectsRef = useRef(null);
